f47ff1e349d09d18619701b9847e3c44f9c66bef
e5ash <e5ash.bro@gmail.com>
2018-07-05 12:02:04 +0300

    main page

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,6 +1,44 @@
 $(document).ready(function($) {
 
-	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
+	$('.input_phone input').mask('+7 (000) 000-00-00');
 
 	
+  $('.date input').datepicker({
+  	firstDay: 1,
+  	dateFormat: "dd.mm.yy",
+  	dayNamesMin: [ "Во", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ],
+  	dayNames:[ "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" ],
+  	monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ]
+  });
+
+  $('.select select').selectmenu();
+
+
+  $('.reviews__list').slick({
+  	fade: true
+  });
+
+  $(".nano").nanoScroller();
+
+  $('.plans__list').each(function(index, el) {
+  	var img = $(this).find('.plans__img'),
+  			imgs = $(this).find('.plans__imgs');
+  
+	  img.slick({
+	  	arrows: false,
+	  	slidesToShow: 1,
+	  	dots: true,
+	  	asNavFor: imgs,
+	  	fade: true
+	  })
+	  imgs.slick({
+	  	slidesToShow: 3,
+	  	vertical: true,
+	  	arrows: false,
+	  	asNavFor: img,
+	  	focusOnSelect: true
+	  })
+  });
+
+  $('.prices__wrap').tabs();
 });
